fb0cb7f0f1b6713b72845d35c1486618fa8b0f2b
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-05-25 22:47:50 +0530

    needle glitch

--- Clock/script.js
+++ Clock/script.js
@@ -66,7 +66,7 @@ function setTime() {
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     minutes,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
@@ -74,7 +74,7 @@ function setTime() {
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     seconds,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
