36febcda26a368d272778822ca754766a1685c8d
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-05-27 14:49:49 +0530

    console remove

--- Drag And Drop/script.js
+++ Drag And Drop/script.js
@@ -8,38 +8,32 @@ empties.forEach((empty) => {
   empty.addEventListener("dragover", dragOver);
   empty.addEventListener("dragenter", dragEnter);
   empty.addEventListener("dragleave", dragLeave);
-  empty.addEventListener("dragdrop", dragDrop);
+  empty.addEventListener("drop", dragDrop);
 });
 
 function dragStart() {
   this.className += " hold";
   setTimeout(() => (this.className = "invisible"), 0);
-  console.log("dragStart");
 }
 
 function dragEnd() {
   this.className = "fill";
-  console.log("dragEnd");
 }
 
 function dragOver(e) {
   e.preventDefault();
-  console.log("dragOver");
 }
 
 function dragEnter(e) {
   e.preventDefault();
   this.className += " hovered";
-  console.log("dragEnter");
 }
 
 function dragLeave() {
   this.className = "empty";
-  console.log("dragLeave");
 }
 
 function dragDrop() {
   this.className = "empty";
   this.append(fill);
-  console.log("dragDrop");
 }
