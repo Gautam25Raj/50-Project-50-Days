84d04df26286011a0c4e6bb10de7e6677b92ffff
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-05-30 01:15:30 +0530

    js

--- Double Verticle Slider/script.js
+++ Double Verticle Slider/script.js
@@ -1,94 +1,47 @@
-const hourEl = document.querySelector(".hour");
-const minuteEl = document.querySelector(".minute");
-const secondEl = document.querySelector(".second");
-const timeEl = document.querySelector(".time");
-const dateEl = document.querySelector(".date");
-const toggle = document.querySelector(".toggle");
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slideLenght = slideRight.querySelectorAll("div").length;
 
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+let activeSlideIndex = 0;
 
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+slideLeft.style.top = `-${(slideLenght - 1) * 100}vh`;
 
-toggle.addEventListener("click", (e) => {
-  const html = document.querySelector("html");
-  //   html.classList.toggle("dark");
-
-  if (html.classList.contains("dark")) {
-    e.target.innerHTML = "Darl Mode";
-    html.classList.remove("dark");
-  } else {
-    html.classList.add("dark");
-    e.target.innerHTML = "Light Mode";
-    html.classList.add("dark");
-  }
+upButton.addEventListener("click", () => {
+  changeSlide("up");
 });
 
-function setTime() {
-  const time = new Date();
-  const month = time.getMonth();
-  const day = time.getDay();
-  const date = time.getDate();
-  const hours = time.getHours();
-  const hoursForClock = hours >= 13 ? hours % 12 : hours;
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
-  const ampm = hours >= 12 ? "PM" : "AM";
+downButton.addEventListener("click", () => {
+  changeSlide("down");
+});
 
-  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-    hoursForClock,
-    0,
-    12,
-    0,
-    360
-  )}deg)`;
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
 
-  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-    minutes,
-    0,
-    60,
-    0,
-    360
-  )}deg)`;
+  if (direction === "up") {
+    activeSlideIndex++;
+    console.log("1");
 
-  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
-    seconds,
-    0,
-    60,
-    0,
-    360
-  )}deg)`;
+    if (activeSlideIndex > slideLenght - 1) {
+      activeSlideIndex = 0;
+    }
+  }
 
-  timeEl.innerHTML = `${hoursForClock}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  } ${ampm}`;
+  if (direction === "down") {
+    activeSlideIndex--;
 
-  dateEl.innerHTML = `${days[day]} , ${months[month]} <span class="circle">${date}</span>`;
-}
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slideLenght - 1;
+    }
+  }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
 };
-setTime();
-
-setInterval(setTime, 1000);
